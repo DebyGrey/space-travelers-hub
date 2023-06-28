3899fea87c26a74b5cfc493189584cef9808b156
Martin Karugaba <martinkarugaba21@gmail.com>
2023-06-28 20:31:36 +0300

    Adjust navbar padding

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png';
 const Logo = () => (
   <div className="flex justify-start items-center gap-x-4">
     <img src={logo} className="h-[60px] " alt="logo" />
-    <h1 className="text-3xl font-semibold">
+    <h1 className="text-3xl font-bold">
       Space Travelers&apos; Hub
     </h1>
   </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
     { id: 3, name: 'My-profile', path: 'my-profile' },
   ];
   return (
-    <div className="px-[3.5rem]">
-      <div className="font-manrope border-b flex justify-between items-center py-[1rem]">
+    <div className="px-[3.2rem] font-manrope">
+      <div className="border-b flex justify-between items-center px-[1.8rem] py-[1.5rem]">
         <Logo />
         <nav className="nav-left ">
           <ul className="flex justify-end items-center">
