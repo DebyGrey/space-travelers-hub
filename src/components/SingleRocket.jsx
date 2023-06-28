9b72b15f6558921117ca3a16c5a6b858d9ee040e
Martin Karugaba <martinkarugaba21@gmail.com>
2023-06-28 19:00:07 +0300

    Add height to nav-items

--- src/components/SingleRocket.jsx
+++ src/components/SingleRocket.jsx
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types';
 
 const SingleRocket = ({ description, rocketName, flickrImages }) => (
-  <div className="flex justify-start items-start mb-[2.5rem] gap-x-[1.5rem]">
+  <div className="border h-auto flex justify-start items-start mb-[2.5rem] gap-x-[1.5rem]">
     <img src={flickrImages} className="w-[300px]" alt="rocket" />
-    <div>
+    <div className="py-4">
       <h1 className="text-xl font-medium">{rocketName}</h1>
       <p className="text-base mt-2">{description}</p>
-      <button type="button" className="text-white bg-blue-600 cursor-pointer mt-3 rounded px-[1rem] py-2">Reserve rocket</button>
+      <button
+        type="button"
+        className="text-white bg-blue-600 cursor-pointer mt-3 rounded px-[1rem] py-2"
+      >
+        Reserve rocket
+      </button>
     </div>
   </div>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        manrope: ["Manrope", "sans-serif"],
+        manrope: ['Manrope', 'sans-serif'],
       },
     },
   },
