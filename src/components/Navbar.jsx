85578fcb7afb75d45275429a817e332a09e87f0d
Martin Karugaba <martinkarugaba21@gmail.com>
2023-06-28 09:31:31 +0300

    Add basic styles to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,22 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => (
   <>
-    <div className="navbar border">
-      <div className="nav-left">
-        <h1>Space Travelers&apos; Hub</h1>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Rockets</NavLink>
-            </li>
-            <li>
-              <NavLink to="missions">Missions</NavLink>
-            </li>
-            |
-            <li>
-              <NavLink to="my-profile">My Profile</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div className="navbar border flex justify-between items-center px-[5rem] py-[2rem]">
+      <h1>Space Travelers&apos; Hub</h1>
+      <nav className="nav-left ">
+        <ul className="flex justify-end items-center gap-x-4">
+          <li>
+            <NavLink to="/">Rockets</NavLink>
+          </li>
+          <li>
+            <NavLink to="missions">Missions</NavLink>
+          </li>
+          |
+          <li>
+            <NavLink to="my-profile">My Profile</NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   </>
 );
