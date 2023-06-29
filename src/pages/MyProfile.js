bc0aa486dbf1a5b272a93109f06921b1d963d1fc
Martin Karugaba <martinkarugaba21@gmail.com>
2023-06-29 15:29:29 +0300

    Increase reserved rocket list border radius

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -9,13 +9,13 @@ const MyProfile = () => {
 
   return (
     <div className="px-[5rem] h-auto py-[2rem] font-manrope flex justify-between items-center gap-x-[3rem]">
-      <div className="border w-1/2 h-auto rounded">
+      <div className="border w-1/2 h-auto rounded-md">
         <h2>My missions</h2>
         <div />
       </div>
       <div className="w-1/2 h-auto">
         <h2 className="text-3xl font-bold mb-3">My Rockets</h2>
-        <ul className="border rounded list-none">
+        <ul className="border rounded-md list-none">
           {reservedRockets.map((rocket) => (
             <li className="pb-8 pt-4 pl-8 border-b font-normal" key={rocket.id}>
               {rocket.rocket_name}
